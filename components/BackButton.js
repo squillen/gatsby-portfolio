df6ca47cebeeed9c43989b44aa63a0bd9d6f8701
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-19 14:13:56 -0600

    add back button

---
+++ components/BackButton.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import backArrow from '../src/images/svgs/straight-arrow-outline.svg'
+
+const BackButton = () => {
+  return (
+    <Link to="/">
+      <img className="back-arrow back" src={backArrow} alt="back button" />
+    </Link>
+  )
+}
+
+export default BackButton
