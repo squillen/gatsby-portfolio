b19d3f452e07da6830e62fa2967406d9453916b5
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-28 10:28:18 -0600

    move say hi to above row

--- components/Home.js
+++ components/Home.js
@@ -12,22 +12,6 @@ import envelope from '../src/images/svgs/icons/envelope.svg';
 import github from '../src/images/svgs/icons/github.svg';
 import linkedin from '../src/images/svgs/icons/linkedin.svg';
 
-const ContactSection = () => {
-  return (
-    <section className="section-container-contact">
-          <div className="section-text-container">
-            <h2 className="description-text">say hi</h2>
-            <img className="arrow rotate90 about-arrow" src={straightArrow} alt="arrow pointing left to the word 'writes'"/>
-          </div>
-          <div className="contact-icons-container">
-            <a href="https://www.github.com/squillen"><img className="contact-logo" src={github} alt="link to my github"/></a>
-            <a href="https://www.linkedin.com/in/sean-quillen/"><img className="contact-logo" src={linkedin} alt="link to my linkedin"/></a>
-            <a href="mailto: sean.r.quillen@gmail.com"><img className="contact-logo" src={envelope} alt="email me"/></a>
-          </div>
-        </section>
-  )
-}
-
 const Home = () => {
   return (
     <section className="section-container">
@@ -56,7 +40,17 @@ const Home = () => {
           {/* PHOTOS PICTURE */}
           <Link data-hover="photos." className="section-block-photos" to="/photos"></Link>
         </section>
-        <ContactSection />
+        <section className="section-container-contact">
+          <div className="section-text-container">
+            <h2 className="description-text">say hi</h2>
+            <img className="arrow rotate90 about-arrow" src={straightArrow} alt="arrow pointing left to the word 'writes'"/>
+          </div>
+          <div className="contact-icons-container">
+            <a href="https://www.github.com/squillen"><img className="contact-logo" src={github} alt="link to my github"/></a>
+            <a href="https://www.linkedin.com/in/sean-quillen/"><img className="contact-logo" src={linkedin} alt="link to my linkedin"/></a>
+            <a href="mailto: sean.r.quillen@gmail.com"><img className="contact-logo" src={envelope} alt="email me"/></a>
+          </div>
+        </section>
       </div>
 
 
@@ -80,20 +74,21 @@ const Home = () => {
             </div>
         </section>
         <section className="section-container-photos">
+          <div className="section-text-container">
+            <h2 className="description-text">say hi</h2>
+            <img className="arrow rotate90 contact-arrow" src={slightCurve} alt="arrow pointing left to the word 'writes'"/>
+          </div>
           {/* PHOTOS PICTURE */}
           <Link data-hover="photos." className="section-block-photos" to="/photos"></Link>
         </section>
-        <ContactSection />
+        <section className="section-container-contact">
+          <div className="contact-icons-container">
+            <a href="https://www.github.com/squillen"><img className="contact-logo" src={github} alt="link to my github"/></a>
+            <a href="https://www.linkedin.com/in/sean-quillen/"><img className="contact-logo" src={linkedin} alt="link to my linkedin"/></a>
+            <a href="mailto: sean.r.quillen@gmail.com"><img className="contact-logo" src={envelope} alt="email me"/></a>
+          </div>
+        </section>
       </div>
-
-      {/* <section className="section-main">
-        <a href="#code"><section className="section-block-code">code</section></a>
-        <a href="#photos"><section className="section-block-photos">photos</section></a>
-        <a href="#about"><section className="section-block-about">about</section></a>
-      </section>
-      <section id="code"className="section-code">code</section>
-      <section id="photos" className="section-photos">photos</section>
-      <section id="about" className="section-about">about</section> */}
     </section>
   )
 }
