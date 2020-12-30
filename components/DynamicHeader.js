768e6d904ccbaa28e79ac4301242e783874484c7
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-30 14:25:53 -0600

    fade in arrows

--- components/DynamicHeader.js
+++ components/DynamicHeader.js
@@ -14,15 +14,15 @@ const DynamicHeader = ({ header, subHeader, id = '', headerArrowRight, subHeader
       <BackButton />
       <div className="center">
         <div className="header-container">
-          {!headerArrowRight && <img src={leftArrow} alt="" className="arrow down rotate90 arrow-header-left" />}
+          {!headerArrowRight && <img src={leftArrow} alt="left arrow" className="arrow down rotate90 arrow-header-left" />}
           <h1 className="heading-primary--main">{header}</h1>
-          {headerArrowRight && <img src={rightArrow} alt="" className="arrow down rotate75 arrow-header-right" />}
+          {headerArrowRight && <img src={rightArrow} alt="right arrow" className="arrow down rotate75 arrow-header-right" />}
         </div>
         {subHeader && (
           <div className={`sub-header-container-${subHeaderRight ? 'right' : 'left'}`}>
-            {!subHeaderRight && <img src={leftArrow} alt="" className="arrow down rotate75 arrow-header-left" />}
+            {!subHeaderRight && <img src={leftArrow} alt="left arrow" className="arrow down rotate75 arrow-header-left" />}
             <h2 className="heading-primary--sub">{subHeader}</h2>
-            {subHeaderRight && <img src={rightArrow} alt="" className="arrow down rotate75 arrow-header-right" />}
+            {subHeaderRight && <img src={rightArrow} alt="right arrow" className="arrow down rotate75 arrow-header-right" />}
           </div>
         )}
       </div>
